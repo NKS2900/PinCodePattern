099dae40d56079c1164a7c46a79e67b756cdcaf9
unknown <nksayyad2900.com>
2020-10-14 11:10:55 +0530

    Pincode Validation pattern

---
+++ Pincode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash +x
+echo "------PinCode Pattern-------"
+shopt -s extglob
+read -p "Enter pincode no: " pincode
+
+format='^[0-9]{6}$'
+
+if [[ $pincode =~ $format ]]
+then
+	echo "Valid Pincode : " $pincode
+else
+	echo "$pincode is not Valid PinCode..."
+fi
